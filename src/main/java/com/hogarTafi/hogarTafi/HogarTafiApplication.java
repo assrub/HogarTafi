9ec5f18a7138b49557b067efaa2efd55f9da4800
aguslebed <agus.lebed@gmail.com>
2024-04-26 02:01:22 -0300

    otra prueba

--- src/main/java/com/hogarTafi/hogarTafi/HogarTafiApplication.java
+++ src/main/java/com/hogarTafi/hogarTafi/HogarTafiApplication.java
@@ -2,12 +2,19 @@ package com.hogarTafi.hogarTafi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HogarTafiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HogarTafiApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(){
+		return String.format("Hello world!");
+	}
 }
