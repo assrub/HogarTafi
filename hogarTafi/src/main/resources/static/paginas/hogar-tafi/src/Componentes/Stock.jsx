4f66f5b49dba2848395b9c3fe1a3ca034563a912
aguslebed <agus.lebed@gmail.com>
2024-07-18 15:44:53 -0300

    a

--- hogarTafi/src/main/resources/static/paginas/hogar-tafi/src/Componentes/Stock.jsx
+++ hogarTafi/src/main/resources/static/paginas/hogar-tafi/src/Componentes/Stock.jsx
@@ -6,7 +6,7 @@ export default function Stock() {
   return (
     <div>
         <div className="titulo flex text-neutral-300 text-4xl justify-center m-10">
-            <h2>Stock de pepe </h2>    
+            <h2>Stock de pepee </h2>    
         </div>
         <div className="tabla">
           <TablaStock></TablaStock>
