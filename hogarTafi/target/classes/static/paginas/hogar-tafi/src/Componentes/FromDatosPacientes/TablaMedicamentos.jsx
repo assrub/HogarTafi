d2c70b5f87b159c654d1f26fae5e3aeac4d66f44
debenedettohernan <hernan.debenedetto@hotmail.com>
2024-08-19 01:31:43 -0300

    todo

---
+++ hogarTafi/target/classes/static/paginas/hogar-tafi/src/Componentes/FromDatosPacientes/TablaMedicamentos.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const TablaMedicamentos = () => {
+  const diasDeLaSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+  const horasDelDia = [];
+  
+
+  for (let i = 0; i < 24; i += 3) {
+    horasDelDia.push(`${i}:00`);
+  }
+
+  return (
+    <div className="overflow-x-auto rounded-xl">
+      <table className="min-w-full bg-white ">
+        <thead>
+          <tr>
+            <th className="px-4 py-2 border border-[#181818]">Hora/Día</th>
+            {diasDeLaSemana.map((dia, index) => (
+              <th key={index} className="px-4 py-2 border border-[#181818]">{dia}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {horasDelDia.map((hora, index) => (
+            <tr key={index}>
+              <td className="px-4 py-2 border border-[#181818]">{hora}</td>
+              {diasDeLaSemana.map((dia, index) => (
+                <td key={index} className="px-4 py-2 border border-[#181818]"></td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TablaMedicamentos;
