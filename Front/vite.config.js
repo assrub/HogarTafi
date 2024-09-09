05f25654b541a8d39ec5530b03d9ac7d25f1e356
assrub <rubdario23@gmail.com>
2024-09-08 22:00:26 -0300

    Se corrigio el dockerfile para que vite se actualice de forma automatica y se borraron carpetas inecesarias

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, //obligas a Vite a verificar periódicamente si los archivos han cambiado  
+      interval: 100, // Intervalo en milisegundos (ajústalo según tu preferencia)
+    },
+    host: true, // Asegúrate de que el servidor esté accesible desde Docker
+    strictPort: true, // Mantiene el puerto establecido sin cambiar
+    port: 5173, // El puerto que expones en tu contenedor
+  },
 })
--- back/src/main/resources/static/paginas/hogar-tafi/src/Componentes/Barra de navegacion/Enlace.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function Enlace( {texto, enlace} ) {
-  return (
-    <a href={enlace} className="block px-2 py-2 text-neutral-300 hover:bg-gray-200 hover:text-gray-800 hover:rounded-lg"><span>{texto}</span></a>
-  )
-}
-
-export default Enlace
