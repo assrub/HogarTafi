01e4448dec53f82db212dc3371f8e0b8af8b782b
aguslebed <agus.lebed@gmail.com>
2024-10-12 18:47:35 -0300

    Boton agregar tabla stock

--- Front/src/componentes/FormPacientes/TablaStock.jsx
+++ Front/src/componentes/FormPacientes/TablaStock.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect, useState, forwardRef } from "react";
+import React, { useEffect,useState, forwardRef } from "react";
 import { traerStockApi } from "../../api";
 
-const TablaStock = forwardRef(({ dni }, ref) => {
-  // Inicializa con una fila vacía y editable
-  const [rows, setRows] = useState([{ medicacion: '', cantidad: '', cantidadMinima: '', added: false, isEditing: true }]);
-  
+const TablaStock =forwardRef(({dni}, ref) => {
+  const [rows, setRows] = useState([{ medicacion: '', cantidad: '', cantidadMinima: '', added: false }]);
+  const [nuevoStock, setNuevoStock] = useState({ medicacion: '', cantidad: '', cantidadMinima: '', added: false });
+
+
+
   const handleInputChange = (index, event) => {
     const { target } = event;
-
+  
     if (target && target.name) {
       const { name, value } = target;
-
+  
       // Actualiza la fila correspondiente con el valor nuevo
       setRows((prevRows) => {
         const updatedRows = [...prevRows];
@@ -26,11 +28,32 @@ const TablaStock = forwardRef(({ dni }, ref) => {
   };
 
   const handleAddRow = () => {
-    setRows((prevRows) => [
-      ...prevRows,
-      { medicacion: '', cantidad: '', cantidadMinima: '', added: false, isEditing: true }
-    ]);
+    
+    if (nuevoStock.medicacion && nuevoStock.cantidad && nuevoStock.cantidadMinima) {
+     
+      setRows((prevRows) => [
+        ...prevRows,
+        { 
+          medicacion: nuevoStock.medicacion, 
+          cantidad: nuevoStock.cantidad, 
+          cantidadMinima: nuevoStock.cantidadMinima, 
+          added: true 
+        }
+      ]);
+  
+      
+      setNuevoStock({
+        medicacion: '',
+        cantidad: '',
+        cantidadMinima: '',
+        added: false
+      });
+    } else {
+      
+      alert('Llena todos los campos');
+    }
   };
+
 
   const handleEditRow = (index) => {
     setRows(prevRows => 
@@ -40,6 +63,7 @@ const TablaStock = forwardRef(({ dni }, ref) => {
     );
   };
 
+ 
   const handleSaveEdit = (index) => {
     setRows(prevRows => 
       prevRows.map((row, i) => 
@@ -49,116 +73,169 @@ const TablaStock = forwardRef(({ dni }, ref) => {
   };
 
   const handleRemoveRow = (index) => {
-    setRows((prevRows) => {
-      const newRows = prevRows.filter((_, i) => i !== index);
-      // Asegúrate de que quede al menos una fila
-      if (newRows.length === 0) {
-        newRows.push({ medicacion: '', cantidad: '', cantidadMinima: '', added: false, isEditing: true });
-      }
-      return newRows;
-    });
+    if (rows.length > 1) {
+      const newRows = [...rows];
+      newRows.splice(index, 1);
+      setRows(newRows);
+    } else {
+      handleClearContent(index);
+    }
   };
 
-  async function traerStock(dni) {
-    try {
-      const response = await traerStockApi(dni);
-      if (response) {
-        // Transforma el stock recibido y agrega una fila vacía al final
-        const transformedRows = transformarStock(response.medicamentos);
-        setRows([...transformedRows, { medicacion: "", cantidad: "", cantidadMinima: "", added: false, isEditing: true }]);
-      } else {
-        // Asegura que haya al menos una fila vacía si no hay datos
-        setRows([{ medicacion: "", cantidad: "", cantidadMinima: "", added: false, isEditing: true }]);
+  async function traerStock(dni){
+    try{
+      const response  = await traerStockApi(dni);
+      if(response){
+        setRows(transformarStock(response.medicamentos));
+      }else{
+        setRows([
+          {
+            medicacion : "",
+            cantidad : "",
+            cantidadMinima: "",
+            added: false
+          }
+        ])
       }
-    } catch (error) {
+      
+    
+    }catch(error){
       console.error(error);
     }
   }
-  
+
   useEffect(() => {
     traerStock(dni);
-  }, [dni]);
+  }, []);
 
+  
   function transformarStock(stockBackend) {
     return stockBackend.map(stock => ({
       medicacion: stock.medicacion,
-      cantidad: stock.cantidad,
-      cantidadMinima: stock.cant_minima,
-      added: true,
-      isEditing: false  // Al cargar, no deben estar editables
+      cantidad : stock.cantidad,
+      cantidadMinima : stock.cant_minima,
+      added: true  
     }));
   }
 
+
+  
+
   const handleClearContent = (index) => {
     const newRows = [...rows];
-    newRows[index] = { medicacion: '', cantidad: '', cantidadMinima: '', added: false, isEditing: true };
+    newRows[index] = { medicacion: '', cantidad: '', cantidadMinima: '', added: false };
     setRows(newRows);
   };
 
   return (
-    <>
-      <div className="max-w-[400px] mx-auto p-2 bg-transparent rounded-lg">
-        <table className="table-auto w-full" ref={ref}>
-          <thead>
-            <tr>
-              <th className="px-4 py-2 border border-[#181818]">Medicacion</th>
-              <th className="px-4 py-2 border border-[#181818]">Cantidad</th>
-              <th className="px-4 py-2 border border-[#181818]">Cantidad minima</th>
-              <th className="px-4 py-2 border border-[#181818]">Acciones</th>
+    <div className="overflow-x-auto rounded-xl">
+      <table className=" w-full bg-transparent mb-10" ref={ref}>
+        <thead>
+          <tr>
+            <th className="px-4 py-2 border border-[#181818]">Medicacion</th>
+            <th className="px-4 py-2 border border-[#181818]">Cantidad</th>
+            <th className="px-4 py-2 border border-[#181818]">Cantidad minima</th>
+            <th className="px-4 py-2 border border-[#181818]">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row, index) => (
+            <tr key={index}>
+              <td className="lg:px-4 lg:py-2 border border-[#181818] ">
+                <input
+                  className={`rounded-lg text-center w-32 ${row.isEditing ? 'bg-white' : 'bg-neutral-300'}`}
+                  type="text"
+                  name="medicacion"
+                  value={row.medicacion}
+                  onChange={(event) => handleInputChange(index, event)}
+                  disabled={!row.isEditing}
+                />
+              </td>
+              <td className="lg:px-4 lg:py-2 border border-[#181818] ">
+                <input
+                  className={`rounded-lg text-center w-32 ${row.isEditing ? 'bg-white' : 'bg-neutral-300'}`}
+                  type="number"
+                  name="cantidad"
+                  value={row.cantidad}
+                  onChange={(event) => handleInputChange(index, event)}
+                  disabled={!row.isEditing}
+                />
+              </td>
+              <td className="lg:px-4 lg:py-2 border border-[#181818] ">
+                <input
+                  className={`rounded-lg text-center w-32 ${row.isEditing ? 'bg-white' : 'bg-neutral-300'}`}
+                  type="number"
+                  name="cantidadMinima"
+                  value={row.cantidadMinima}
+                  onChange={(event) => handleInputChange(index, event)}
+                  disabled={!row.isEditing}
+                />
+              </td>
+              <td className="lg:px-4 lg:py-2 border border-[#181818]">
+                <div className="flex place-content-center gap-4">
+                  {row.isEditing ? (
+                    <button className="text-blue-600" onClick={() => handleSaveEdit(index)}>Guardar</button>
+                  ) : (
+                    <button className="text-gray-600" onClick={() => handleEditRow(index)}>Modificar</button>
+                  )}
+                  <button className="text-red-600" onClick={() => handleRemoveRow(index)}>Eliminar</button>
+                </div>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {rows.map((row, index) => (
-              <tr key={index} className={`border border-[#181818] ${row.isEditing ? 'bg-white' : 'bg-gray-300'}`}>
-                <td className="border border-[#181818]">
-                  <input
-                    className={`w-full h-full text-center ${row.isEditing ? 'bg-white' : 'bg-gray-300'} border-none rounded-none`}
-                    type="text"
-                    name="medicacion"
-                    value={row.medicacion}
-                    onChange={(event) => handleInputChange(index, event)}
-                    disabled={!row.isEditing}
-                    style={{ boxSizing: 'border-box', margin: '0', padding: '0' }} 
-                  />
-                </td>
-                <td className="border border-[#181818]">
-                  <input
-                    className={`w-full h-full text-center ${row.isEditing ? 'bg-white' : 'bg-gray-300'} border-none rounded-none`}
-                    type="number"
-                    name="cantidad"
-                    value={row.cantidad}
-                    onChange={(event) => handleInputChange(index, event)}
-                    disabled={!row.isEditing}
-                    style={{ boxSizing: 'border-box', margin: '0', padding: '0' }} 
-                  />
-                </td>
-                <td className="border border-[#181818]">
-                  <input
-                    className={`w-full h-full text-center ${row.isEditing ? 'bg-white' : 'bg-gray-300'} border-none rounded-none`}
-                    type="number"
-                    name="cantidadMinima"
-                    value={row.cantidadMinima}
-                    onChange={(event) => handleInputChange(index, event)}
-                    disabled={!row.isEditing}
-                    style={{ boxSizing: 'border-box', margin: '0', padding: '0' }} 
-                  />
-                </td>
-                <td className="border border-[#181818]">
-                  <div className="flex justify-center gap-2">
-                    {row.isEditing ? (
-                      <button className="text-blue-600" onClick={() => { handleSaveEdit(index); handleAddRow(); }}>Guardar</button>
-                    ) : (
-                      <button className="text-gray-600" onClick={() => handleEditRow(index)}>Modificar</button>
-                    )}
-                    <button className="text-red-600" onClick={() => handleRemoveRow(index)}>Eliminar</button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+          ))}
+
+<tr>
+            <td className="lg:px-4 lg:py-2 border border-[#181818]">
+              <div className="flex place-content-center">
+                <input
+                  className="rounded-lg w-32 flex text-center bg-white border"
+                  type="text"
+                  name="medicacion"
+                  value={nuevoStock.medicacion}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </td>
+            <td className="lg:px-4 lg:py-2 border border-[#181818]">
+              <div className="flex place-content-center">
+                <input
+                  className="rounded-lg w-32 flex text-center bg-white border"
+                  type="number"
+                  name="cantidad"
+                  min={0}
+                  value={nuevoStock.cantidad}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </td>
+            <td className="lg:px-4 lg:py-2 border border-[#181818]">
+              <div className="flex place-content-center">
+                <input
+                  className="rounded-lg w-32 flex text-center bg-white border"
+                  type="number"
+                  name="cantidadMinima"
+                  min={0}
+                  value={nuevoStock.cantidadMinima}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </td>
+            <td className="lg:px-4 lg:py-2 border border-[#181818]">
+              <div className="gap-10 flex place-content-center">
+                <button className="text-green-600 flex" onClick={handleAddRow}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                  </svg>
+                  Agregar
+                </button>
+              </div>
+            </td>
+          </tr>
+
+
+        </tbody>
+      </table>
+    </div>
   );
 });
 
